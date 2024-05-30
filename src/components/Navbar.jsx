f43f88456e6a161e5fcaf344065159f03e9e459a
MuzhdaN <muzhda.noorzad4@gmail.com>
2024-05-29 21:08:26 -0500

    Add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,49 @@
 
+import { NavLink } from 'react-router-dom';
+import reactLogo from '../assets/react.svg';
+
 
 const Navbar = () => {
+
+  const activeLink = ({isActive}) => 
+      isActive 
+      ? 'bg-violet-950 hover:bg-gray-900 rounded-md px-3 py-2' 
+      : 'hover:bg-gray-900 rounded-md px-3 py-2';
+  
+  
   return (
-    <div>Navbar</div>
+    <nav className="bg-violet-700 border-b border-violet-500">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 flex h-20 items-center justify-between">
+            
+            <NavLink to='/' className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+                <img src={reactLogo} className="h-10 w-auto" alt="React Courses Logo" />
+                <span className="hidden md:block text-white text-2xl font-bold ml-2">
+                  My Courses
+                </span>
+            </NavLink>
+
+          <div className='md:ml-auto'>
+            <div className=" flex space-x-2 text-white">
+                <NavLink
+                 to="/" 
+                 className={activeLink}>
+                  Home
+                </NavLink>
+                <NavLink 
+                  to="/courses" 
+                  className={activeLink}>
+                    Courses
+                </NavLink>
+                <NavLink 
+                  to="/add-course"
+                  className={activeLink}>
+                    Add Course
+                </NavLink>
+            </div> 
+            </div> 
+        </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
